b99042d1f0d59c2afae6ec85f87b01bd468bdd61
dpasternacki <91115568+dpasternacki@users.noreply.github.com>
2021-09-28 08:57:20 +0200

    Add files via upload

---
+++ zmienne/script.js
@@ -0,0 +1,15 @@
+//zadanie 1
+var x = 1;
+var nazw = "TGB";
+document.write('Mój komputer ma dysk o pojemności '+x+nazw+"<br><br><br><br><br><br>");
+
+//zadanie 2
+
+var nazwisko ='Pasternacki';
+var imie = "Dawid";
+var wiek = 18;
+var klasa = "3ftg";
+
+document.write('Uczeń naszej szkoły nazywa się '+imie+" "+nazwisko+"<br><br>");
+document.write('Uczęszcza do klasy '+klasa+"<br><br>");
+document.write('Ma '+wiek+' lat');
