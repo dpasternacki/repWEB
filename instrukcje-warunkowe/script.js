63809056c866b7c55708a8b375df87e326118619
dpasternacki <91115568+dpasternacki@users.noreply.github.com>
2021-09-28 10:29:57 +0200

    Add files via upload

---
+++ instrukcje-warunkowe/script.js
@@ -0,0 +1,10 @@
+function zarejestruj(formularz){
+
+    if(formularz.pass.value == formularz.pass2.value)
+    {
+        alert('Hasła zgodne, witamy na stronie :)')
+    }
+    else
+        alert('Hasła nie są zgodne wpisz jeszcze raz')
+
+}
