b6ce65f1b74c34c01cec2fc36138eca827c65a76
dpasternacki <91115568+dpasternacki@users.noreply.github.com>
2021-09-28 08:54:12 +0200

    Add files via upload

---
+++ okna-dialogowe/script.js
@@ -0,0 +1,9 @@
+//zadanie 1
+alert('Zaraz zostanie wyświetlony obrazek')
+
+//zadanie 2
+
+if(confirm("Czy masz 18 lat żeby zobaczyc to zdjecie??")){
+    document.write('<img height="1220px" width="1700px" src="monkey.jpg">');
+}
+else{document.write('Nie mozesz zobaczyc tego zdjecia bo nie masz 18 lat')}
