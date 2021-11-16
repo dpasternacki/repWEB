c682bd916e5cf9cf22d7727cc1179c378f6f0706
dpasternacki <91115568+dpasternacki@users.noreply.github.com>
2021-11-16 10:19:08 +0100

    Add files via upload

---
+++ 16-11/script.js
@@ -0,0 +1,54 @@
+/*function jak_szybko(t){
+    let wynik = 1320 / t;
+    return wynik;
+}
+t = prompt('Podaj czas w godzinach ile leciał samolot');
+alert('Samolot leciał '+jak_szybko(t)+' na godzine');*/
+/*
+function zamiana(m){
+
+    let wynik = 1609 * m;
+    return wynik;
+
+}
+m = prompt('Podaj mile');
+alert(m+' mil to '+zamiana(m)+' km')*/
+/*
+function godzina(n){
+
+    let wynik = (21+n)%24;
+    return wynik;
+
+}
+n = prompt('Podaj godzine');
+alert(godzina(n));*/
+/*
+function godz_nj(g){
+
+ let wynik = g - 6
+ if(wynik < 0)
+ {
+     wynik = 24 + wynik
+ }
+return wynik;
+}
+g = prompt('Podaj pełną godzine w Warszawie');
+alert(godz_nj(g))*/
+/*
+function ile_pol(n){
+
+    let wynik = n*n;
+    return wynik;
+
+}
+n = prompt('Podaj liczbe pol szachownicy na długość');
+if(n < 2 || n>32){
+    alert('Mozesz podac liczbe od 2 do 32, zresetuj stone zeby znowu wpisac dane');
+}
+else{
+    alert(ile_pol(n));
+}*/
+
+function ile_kolorowych(n, kolor){
+    
+    }
